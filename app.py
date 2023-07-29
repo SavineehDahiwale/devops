b626d3cad0517de9338e81c6f12e128643a80a3c
SavineshDahiwale <savineshdahiwale@gmail.com>
2023-07-29 10:17:05 +0530

    app.py & Dockerfile

--- app.py
+++ app.py
@@ -13,21 +13,12 @@ def root():
 
 @app.route("/modules", methods=["GET"])
 def modules():
-    return "Ditiss module list :-"
-    return "Compliance Audit"
-    return "Public key Infrastructure"
-    return "Concept of Operating system & Administration"
-    return "Fundamentals of Computer Networking"
-    return "Network Defence & Countermeasures"
-    return "Cyber Forensics"
-    return "Security Concepts"
-    return "ItIL & Devops"
+    return "Ditiss module list :- ItIL & Devops,Security Concepts,Cyber Forensics,Network Defence & Countermeasures,Fundamentals of Computer Networking,Concept of Operating system & Administration,Public key Infrastructure,Compliance Audit"
+
 
 @app.route("/me", methods=["GET"])
 def name():
-    return "Name :- Savinesh Dahiwale"
-    return "PRN :- 230344223044"
-    return "Phone No :- 7350390313"
+    return "Name :- Savinesh Dahiwale, Phone No :- 7350390313, PRN :- 230344223044"
 
 
 # run the application
